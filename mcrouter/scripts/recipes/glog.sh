ef6e6b838bec9cc556b544f5a4ffb9add825032f
Anton Likhtarov <alikhtarov@fb.com>
2014-06-25 18:54:58 -0700

    Open source script fix for gflags
    Summary: gflags.so doesn't contain correct RPATH to find glog.so, so force the linker to include it
    
    Test Plan: install_facebook.sh works now
    
    Reviewed By: gunjan@fb.com
    
    FB internal diff: D1404905

--- mcrouter/scripts/recipes/glog.sh
+++ mcrouter/scripts/recipes/glog.sh
@@ -5,8 +5,6 @@ source common.sh
 grab https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz
 tar xzvf glog-0.3.3.tar.gz
 cd glog-0.3.3
-LD_LIBRARY_PATH="$INSTALL_DIR/lib:$LD_LIBRARY_PATH" \
-    LD_RUN_PATH="$INSTALL_DIR/lib" \
-    LDFLAGS="-L$INSTALL_DIR/lib" \
+LDFLAGS="-Wl,-rpath=$INSTALL_DIR/lib,--enable-new-dtags -L$INSTALL_DIR/lib" \
     CPPFLAGS="-I$INSTALL_DIR/include" \
     ./configure --prefix="$INSTALL_DIR" && make $MAKE_ARGS && make install $MAKE_ARGS
