480c966be6f7f4e2860ca6fcfec15d9d153060e8
Anton Likhtarov <alikhtarov@fb.com>
2014-06-30 12:09:07 -0700

    More open source build fixes
    Summary:
    - Added missing packages to Ubuntu 12.04 script
    - Use gtest-1.6.0 from folly installation
    - Depend on libthrift directly
    
    Test Plan: make distclean && ./scripts/install_facebook.sh
    
    Reviewed By: pavlo@fb.com
    
    FB internal diff: D1411387

--- mcrouter/scripts/install_ubuntu_12.04.sh
+++ mcrouter/scripts/install_ubuntu_12.04.sh
@@ -4,17 +4,21 @@ set -ex
 
 [ -n "$1" ] || ( echo "Install dir missing"; exit 1 )
 
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository ppa:boost-latest/ppa
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:boost-latest/ppa
 sudo apt-get update
 
-sudo apt-get install gcc-4.8 g++-4.8 libboost1.54-dev libboost-thread1.54-dev \
-    ragel autoconf unzip libsasl2-dev git libtool python-dev cmake libssl-dev \
-    libcap-dev libevent-dev libgtest-dev libsnappy-dev scons flex bison \
-    libkrb5-dev binutils-dev
+sudo apt-get install -y gcc-4.8 g++-4.8 libboost1.54-dev libboost-thread1.54-dev \
+    libboost-filesystem1.54-dev libboost-system1.54-dev libboost-regex1.54-dev \
+    libboost-python1.54-dev libboost-context1.54-dev ragel autoconf unzip \
+    libsasl2-dev git libtool python-dev cmake libssl-dev libcap-dev libevent-dev \
+    libgtest-dev libsnappy-dev scons flex bison libkrb5-dev binutils-dev make \
+    libnuma-dev
 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-sudo update-alternatives --install /usr/bin/g++ gcc /usr/bin/g++-4.8 50
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+
+cd $(dirname $0)
 
 ./get_and_build_everything.sh ubuntu-12.04 $1 $2
