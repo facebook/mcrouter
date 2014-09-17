384134845e7ca5c39268b8ddd2b9d95ddff3aa98
Anton Likhtarov <alikhtarov@fb.com>
2014-09-17 13:33:35 -0700

    Don't call mcrouter --help at the end of the build script
    Summary: mcrouter --help exits with nonzero status code, which causes the whole script to have nonzero code.
    
    Test Plan: verify the script has 0 status on successful build
    
    Reviewed By: pavlo@fb.com
    
    Subscribers: meyering, alikhtarov
    
    FB internal diff: D1561594
    
    Tasks: 5175834

--- mcrouter/scripts/get_and_build_everything.sh
+++ mcrouter/scripts/get_and_build_everything.sh
@@ -21,4 +21,4 @@ for script in $(ls "order_$ORDER/" | egrep '^[0-9]+_.*[^~]$' | sort -n); do
 	"./order_$ORDER/$script" "$PKG_DIR" "$INSTALL_DIR" "$MAKE_ARGS"
 done
 
-"$INSTALL_DIR/bin/mcrouter" --help
+printf "%s\n" "Mcrouter installed in $INSTALL_DIR/bin/mcrouter"
