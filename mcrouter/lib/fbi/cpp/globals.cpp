ee54be7aad35eb2a33fa3251686b96c5da33f542
Alecs King <int@fb.com>
2014-11-11 13:53:09 -0800

    Use folly::IPAddress::hash instead of gethostid
    Summary: find a non-loopback ipv4 or ipv6 address and feed it to folly::IPAddress::hash
    
    Test Plan:
    1)
    fbconfig -r mcrouter
    fbmake runtests
    
    2)
    compare hostids on different hosts
    
    Reviewed By: pavlo@fb.com
    
    Subscribers: njormrod, folly-diffs@, trunkagent, ps, bmatheny, alikhtarov
    
    FB internal diff: D1668944
    
    Tasks: 5557721
    
    Signature: t1:1668944:1415736928:fb4b042a575c0b00f52780f3abf54bf7630b3a97

--- mcrouter/lib/fbi/cpp/globals.cpp
+++ mcrouter/lib/fbi/cpp/globals.cpp
@@ -10,10 +10,54 @@
 
 #include <unistd.h>
 
+#include <folly/IPAddress.h>
+
+#include "mcrouter/lib/fbi/cpp/LogFailure.h"
+#include "mcrouter/lib/fbi/network.h"
+
 namespace facebook { namespace memcache { namespace globals {
 
+namespace {
+
+bool getAddrHash(const sockaddr *addr, void *ctx) {
+  try {
+    folly::IPAddress ip(addr);
+    if (!ip.isLoopback() && !ip.isLinkLocal()) {
+      auto result = (uint32_t*) ctx;
+      result[0] = 1;
+      result[1] = ip.hash();
+      return false;
+    }
+  } catch (...) {
+    return true;
+  }
+
+  return true;
+}
+
+// get hash from the first non-loopback/non-linklocal ip4/6 address
+uint32_t getHash() {
+  uint32_t result[2] = {0};
+
+  if (!for_each_localaddr(getAddrHash, result)) {
+    failure::log("mcrouter", failure::Category::kSystemError,
+                 "Can not enumerate local addresses: {}", strerror(errno));
+    return 0;
+  }
+
+  if (result[0] == 0) {
+    failure::log("mcrouter", failure::Category::kBadEnvironment,
+                 "Can not find a valid ip addresss");
+    return 0;
+  }
+
+  return result[1];
+}
+
+}  // anonymous namespace
+
 uint32_t hostid() {
-  static uint32_t h = gethostid();
+  static uint32_t h = getHash();
   return h;
 }
 
