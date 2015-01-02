02710b70255f80f979332504868ee9dba9f67bc3
Andre Pinto <aap@fb.com>
2015-01-02 14:12:39 -0800

    Tiny cleanup
    Summary: Code cleanup following pavlo's diff
    
    Test Plan: unit tests
    
    Reviewed By: pavlo@fb.com
    
    Subscribers: trunkagent, alikhtarov, pavlo
    
    FB internal diff: D1760399

--- mcrouter/routes/McExtraRouteHandleProvider.cpp
+++ mcrouter/routes/McExtraRouteHandleProvider.cpp
@@ -1,10 +1,11 @@
-/**
+/*
  *  Copyright (c) 2014, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
  *  LICENSE file in the root directory of this source tree. An additional grant
  *  of patent rights can be found in the PATENTS file in the same directory.
+ *
  */
 #include "McExtraRouteHandleProvider.h"
 
@@ -29,8 +30,8 @@ McrouterRouteHandlePtr McExtraRouteHandleProvider::makeShadow(
   folly::StringPiece shadowPolicy) {
 
   if (shadowPolicy == "default") {
-    return makeShadowRouteDefault(destination, data, indexInPool,
-                                  DefaultShadowPolicy());
+    return makeShadowRouteDefault(std::move(destination), std::move(data),
+                                  indexInPool, DefaultShadowPolicy());
   } else {
     throw std::logic_error("Invalid shadow policy: " + shadowPolicy.str());
   }
