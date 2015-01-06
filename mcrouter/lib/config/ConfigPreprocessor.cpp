ada99d16aad51c8579d126e3cc6e1ef83d20e572
Pavlo Kushnir <pavlo@fb.com>
2015-01-06 07:35:37 -0800

    Allow multi-select from objects in ConfigPreprocessor
    Summary: now we can select multiple keys from an object (to create an object with some subset of properties).
    
    Test Plan: unit test attached
    
    Reviewed By: int@fb.com
    
    Subscribers: trunkagent, alikhtarov
    
    FB internal diff: D1764301

--- mcrouter/lib/config/ConfigPreprocessor.cpp
+++ mcrouter/lib/config/ConfigPreprocessor.cpp
@@ -10,6 +10,7 @@
 #include "ConfigPreprocessor.h"
 
 #include <random>
+#include <unordered_set>
 
 #include <folly/Format.h>
 #include <folly/json.h>
@@ -444,30 +445,62 @@ class ConfigPreprocessor::BuiltIns {
   }
 
   /**
-   * Selects element from list/object.
-   * Usage: @select(obj,string) or @select(list,int)
+   * Selects element(s) from list/object.
+   * Usage: @select(obj,string/array of strings) or
+   *        @select(list,int/array of ints)
    *
-   * Returns value of corresponding object property/list element
+   * Returns value(s) of corresponding object property(ies)/list element(s)
    */
   static dynamic selectMacro(Context ctx) {
     auto& dictionary = ctx.at("dictionary");
-    const auto& key = ctx.at("key");
+    auto& key = ctx.at("key");
 
     checkLogic(dictionary.isObject() || dictionary.isArray(),
                "Select: dictionary is not array/object");
 
     if (dictionary.isObject()) {
-      checkLogic(key.isString(),
-                 "Select: dictionary is an object, key is not a string");
-      // key should be in dictionary
-      return moveGet(dictionary, key, "Select");
+      checkLogic(key.isString() || key.isArray(),
+                 "Select: dictionary is an object, key is not array/string");
+      if (key.isString()) {
+        // key should be in dictionary
+        return moveGet(dictionary, key, "Select");
+      } else { // array
+        dynamic result = dynamic::object();
+        for (size_t i = 0; i < key.size(); ++i) {
+          auto& it = key[i];
+          checkLogic(it.isString(),
+                     "Select: dictionary is an object, key item is not a "
+                     "string");
+          checkLogic(result.find(it) == result.items().end(),
+                     "Select: duplicate key");
+          auto val = moveGet(dictionary, it, "Select");
+          result.insert(std::move(it), std::move(val));
+        }
+        return result;
+      }
     } else { // array
-      checkLogic(key.isInt(),
-                 "Select: dictionary is an array, key is not an integer");
-      auto id = key.getInt();
-      checkLogic(id >= 0 && size_t(id) < dictionary.size(),
-                 "Select: index out of range");
-      return std::move(dictionary[id]);
+      checkLogic(key.isInt() || key.isArray(),
+                 "Select: dictionary is an array, key is not array/integer");
+      if (key.isInt()) {
+        auto id = key.getInt();
+        checkLogic(id >= 0 && size_t(id) < dictionary.size(),
+                   "Select: index out of range");
+        return std::move(dictionary[id]);
+      } else { // array
+        dynamic result = {};
+        std::unordered_set<size_t> indicies;
+        for (size_t i = 0; i < key.size(); ++i) {
+          checkLogic(key[i].isInt(),
+                     "Select: dictionary is an array, key item is not an "
+                     "integer");
+          auto id = key[i].getInt();
+          checkLogic(id >= 0 && size_t(id) < dictionary.size(),
+                     "Select: index out of range");
+          checkLogic(indicies.insert(id).second, "Select: duplicate index");
+          result.push_back(std::move(dictionary[id]));
+        }
+        return result;
+      }
     }
   }
 
