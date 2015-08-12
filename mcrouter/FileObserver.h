de7a304842e238e6c2f6dbeca967cb26fed85f79
Andre Pinto <aap@fb.com>
2015-08-12 15:56:11 -0700

    Remove PeriodicTaskScheduler
    Summary: Started using folly's ScopedEventBaseThread to completely remove
    PeriodicTaskScheduler.
    
    Test Plan: Unit tests and manual tests
    
    Reviewed By: @alikhtarov
    
    Differential Revision: D2331676

--- mcrouter/FileObserver.h
+++ mcrouter/FileObserver.h
@@ -12,32 +12,29 @@
 #include <functional>
 #include <string>
 
+namespace folly {
+class EventBase;
+}
+
 namespace facebook { namespace memcache { namespace mcrouter {
 
-class PeriodicTaskScheduler;
-
-/* Starts obsering a file using inotify. */
-class FileObserver {
- public:
-  /**
-   * Starts a periodic thread that watches the given file path for changes.
-   *
-   * @param filePath path to the file to watch (can be a symlink)
-   * @param pollPeriodMs how much to wait between asking inotify if
-   *        any updates happened
-   * @param sleepAfterUpdateMs how much to wait before calling onUpdate
-   *        once an inotify event happens (as a crude protection against
-   *        partial writes race condition).
-   * @param onUpdate callback function to call when there is a update seen
-   * @param fallbackOnError function to call if inotify calls fails
-   * @return true on success, false on failure
-   */
-  static bool startObserving(const std::string& filePath,
-                             PeriodicTaskScheduler& taskScheduler,
-                             uint32_t pollPeriodMs,
-                             uint32_t sleepBeforeUpdateMs,
-                             std::function<void(std::string)> onUpdate,
-                             std::function<void()> fallbackOnError = nullptr);
-};
+/**
+ * Starts a periodic thread that watches the given file path for changes.
+ *
+ * @param filePath path to the file to watch (can be a symlink)
+ * @param pollPeriodMs how much to wait between asking inotify if
+ *        any updates happened
+ * @param sleepAfterUpdateMs how much to wait before calling onUpdate
+ *        once an inotify event happens (as a crude protection against
+ *        partial writes race condition).
+ * @param onUpdate callback function to call when there is a update seen
+ * @param fallbackOnError function to call if inotify calls fails
+ */
+bool startObservingFile(const std::string& filePath,
+                        folly::EventBase& evb,
+                        uint32_t pollPeriodMs,
+                        uint32_t sleepBeforeUpdateMs,
+                        std::function<void(std::string)> onUpdate,
+                        std::function<void()> fallbackOnError = nullptr);
 
 }}} // namespace
