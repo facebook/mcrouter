394bfc4180236e31a64728b558a74541f8c55eaf
Adam Chainz <me@adamj.eu>
2016-05-03 10:15:31 -0700

    Fix wangle.sh to look for WANGLE_COMMIT in the right directory.
    Summary:
    Fixes #118.
    Closes https://github.com/facebook/mcrouter/pull/119
    
    Reviewed By: pavlo-fb
    
    Differential Revision: D3253057
    
    Pulled By: jmswen
    
    fb-gh-sync-id: d509c3cf93358b182faf49efd5b2b5b65e1df646
    fbshipit-source-id: d509c3cf93358b182faf49efd5b2b5b65e1df646

--- mcrouter/scripts/recipes/wangle.sh
+++ mcrouter/scripts/recipes/wangle.sh
@@ -5,8 +5,8 @@ source common.sh
 if [[ ! -d wangle ]]; then
   git clone https://github.com/facebook/wangle
   cd "$PKG_DIR/wangle" || die "cd fail"
-  if [[ -f "$REPO_BASE_DIR/fbcode/mcrouter/WANGLE_COMMIT" ]]; then
-    WANGLE_COMMIT="$(head -n 1 "$REPO_BASE_DIR/fbcode/mcrouter/WANGLE_COMMIT")"
+  if [[ -f "$REPO_BASE_DIR/mcrouter/WANGLE_COMMIT" ]]; then
+    WANGLE_COMMIT="$(head -n 1 "$REPO_BASE_DIR/mcrouter/WANGLE_COMMIT")"
     echo "WANGLE_COMMIT file found: using wangle commit $WANGLE_COMMIT"
     git checkout "$WANGLE_COMMIT"
   else
