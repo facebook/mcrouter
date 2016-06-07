c0e61c38d3e183bde686fdd97967e1731c51c5c3
Anton Likhtarov <alikhtarov@fb.com>
2016-06-07 12:10:37 -0700

    Easy: ignore interfaces without addresses
    Summary: for_each_localaddr() should skip over no address interfaces
    
    Reviewed By: andreazevedo
    
    Differential Revision: D3397197
    
    fbshipit-source-id: d596cbf9241c8b1d5cd43ba16d5a967b60555b26

--- mcrouter/lib/fbi/network.c
+++ mcrouter/lib/fbi/network.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2016, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -70,7 +70,7 @@ bool for_each_localaddr(bool (*cb)(const struct sockaddr *addr, void *ctx),
   }
 
   for (it = addrs; it; it = it->ifa_next) {
-    if (!cb(it->ifa_addr, ctx)) {
+    if (it->ifa_addr && !cb(it->ifa_addr, ctx)) {
       break;
     }
   }
