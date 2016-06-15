a54f99cdc54b227563ba728af986f5754d9f482d
Pavlo Kushnir <oracle0809@gmail.com>
2016-06-15 19:58:14 +0300

    [mcrouter] Add build scripts for Ubuntu 16.04

---
+++ mcrouter/scripts/install_ubuntu_16.04.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -ex
+
+[ -n "$1" ] || ( echo "Install dir missing"; exit 1 )
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    autoconf \
+    binutils-dev \
+    bison \
+    cmake \
+    flex \
+    g++ \
+    gcc \
+    git \
+    libboost1.58-all-dev \
+    libdouble-conversion-dev \
+    libevent-dev \
+    libgflags-dev \
+    libgoogle-glog-dev \
+    libjemalloc-dev \
+    libssl-dev \
+    libtool \
+    make \
+    python-dev \
+    ragel
+
+cd "$(dirname "$0")" || ( echo "cd fail"; exit 1 )
+
+./get_and_build_everything.sh ubuntu-16.04 "$@"
