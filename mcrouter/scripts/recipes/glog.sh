f3c56b7a145d9ef154bc474364da650eef15a0fb
Yedidya Feldblum <yfeldblum@fb.com>
2017-03-13 12:05:04 -0700

    No longer use namespace google for gflags
    Summary:
    [Mcrouter] No longer use `namespace google` for `gflags`.
    
    The new thing to use is `namespace gflags`.
    
    Reviewed By: jmswen
    
    Differential Revision: D4663706
    
    fbshipit-source-id: d352931a5b1ac43e0871e0e0f25d5ea100b43943

--- mcrouter/scripts/recipes/glog.sh
+++ mcrouter/scripts/recipes/glog.sh
@@ -6,16 +6,10 @@ if [ ! -d "$PKG_DIR/glog" ]; then
     git clone https://github.com/google/glog.git
     cd "$PKG_DIR/glog" || die "cd fail"
 
-    # dirty hack until the issue is fixed upstream
-    sed -i.bak "s/namespace gflags/namespace GFLAGS_NAMESPACE/g" \
-      src/signalhandler_unittest.cc
-
     autoreconf --install
     LDFLAGS="-Wl,-rpath=$INSTALL_DIR/lib,--enable-new-dtags \
              -L$INSTALL_DIR/lib $LDFLAGS" \
-        CPPFLAGS="-I$INSTALL_DIR/include -DGOOGLE_GLOG_DLL_DECL='' \
-                  -DGFLAGS_NAMESPACE=google \
-                  $CPPFLAGS" \
+        CPPFLAGS="-I$INSTALL_DIR/include -DGOOGLE_GLOG_DLL_DECL='' $CPPFLAGS" \
         ./configure --prefix="$INSTALL_DIR" && \
         make $MAKE_ARGS && make install $MAKE_ARGS
 fi
