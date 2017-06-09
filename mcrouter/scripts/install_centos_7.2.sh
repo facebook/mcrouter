c16579e63202f45a6d4b5e2dc118815b42e4bc02
Theo Najim <tnajim@walmartlabs.com>
2017-06-09 16:04:26 -0700

    Update Centos7 build
    Summary:
    1. Install and use GCC 5.x from Software Collections
    2. Remove ragel and cmake builds since prebuilt binaries are now available
    
    Tested change with Centos 7.2 and Centos 7.3
    Closes https://github.com/facebook/mcrouter/pull/199
    
    Reviewed By: jmswen
    
    Differential Revision: D5216207
    
    Pulled By: glamtechie
    
    fbshipit-source-id: ea6a2ea7a5cfa94bbd0f3968b0e207d1cfe9aed0

--- mcrouter/scripts/install_centos_7.2.sh
+++ mcrouter/scripts/install_centos_7.2.sh
@@ -5,25 +5,35 @@ set -ex
 [ -n "$1" ] || ( echo "Install dir missing"; exit 1 )
 
 sudo yum install -y epel-release
+sudo yum install -y centos-release-scl
+
+# Install devtoolset version 4 for GCC 5.x
+sudo yum install -y devtoolset-4-gcc devtoolset-4-gcc-c++
+
+export CC=/opt/rh/devtoolset-4/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-4/root/usr/bin/c++
 
 sudo yum install -y \
     autoconf \
     binutils-devel \
     bison \
     boost-devel \
+    cmake \
     double-conversion-devel \
     flex \
-    gcc-c++ \
-    gcc \
     git \
     gflags-devel \
     glog-devel \
     jemalloc-devel \
-    openssl-devel \
     libtool \
     libevent-devel \
     make \
-    python-devel 
+    openssl-devel \
+    python-devel \
+    ragel
+
+# Install automake-1.15 from Fedora
+sudo rpm -Uvh "http://dl.fedoraproject.org/pub/fedora/linux/releases/23/Everything/x86_64/os/Packages/a/automake-1.15-4.fc23.noarch.rpm"
 
 cd "$(dirname "$0")" || ( echo "cd fail"; exit 1 )
 
