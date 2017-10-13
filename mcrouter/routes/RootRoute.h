f1f5f61d6dd6eacecafadb52f40e5cab8b792357
Andre Pinto <aap@fb.com>
2017-10-13 15:01:02 -0700

    Set message when miss_on_get_error is applied
    Summary:
    When miss_on_get_error option is applied to a reply, it's usually very hard to
    understand what's going on. This will ensure we set the message field of the
    reply so that it's easy to debug in the future.
    
    Reviewed By: jmswen
    
    Differential Revision: D6035279
    
    fbshipit-source-id: ca5bc63ee521b2183514ffda69218aa2adf42b30

--- mcrouter/routes/RootRoute.h
+++ mcrouter/routes/RootRoute.h
@@ -19,7 +19,9 @@
 #include "mcrouter/ProxyBase.h"
 #include "mcrouter/config.h"
 #include "mcrouter/lib/RouteHandleTraverser.h"
+#include "mcrouter/lib/carbon/RequestReplyUtil.h"
 #include "mcrouter/lib/carbon/RoutingGroups.h"
+#include "mcrouter/lib/mc/msg.h"
 #include "mcrouter/routes/RouteHandleMap.h"
 
 namespace facebook {
@@ -99,7 +101,14 @@ class RootRoute {
         !rh.empty()) {
       /* rh.empty() case: for backwards compatibility,
          always surface invalid routing errors */
+      auto originalResult = reply.result();
       reply = createReply(DefaultReply, req);
+      carbon::setMessageIfPresent(
+          reply,
+          folly::to<std::string>(
+              "Error reply transformed into miss due to miss_on_get_errors. "
+              "Original reply result: ",
+              mc_res_to_string(originalResult)));
     }
     return reply;
   }
