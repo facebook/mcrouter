54fa7b48602fc0f44894d2b4358d2881683edbad
Andre Pinto <aap@fb.com>
2018-01-16 13:19:12 -0800

    Use in_place constructor for loggers
    Reviewed By: glamtechie
    
    Differential Revision: D6713941
    
    fbshipit-source-id: 7f71a12aeed17a8c71ceb518db8a65f78bb49425

--- mcrouter/ProxyRequestContextTyped.h
+++ mcrouter/ProxyRequestContextTyped.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017, Facebook, Inc.
+ *  Copyright (c) 2017-present, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -8,6 +8,8 @@
  *
  */
 #pragma once
+
+#include <folly/Utility.h>
 
 #include "mcrouter/ProxyRequestContext.h"
 #include "mcrouter/ProxyRequestLogger.h"
@@ -140,8 +142,8 @@ class ProxyRequestContextWithInfo : public ProxyRequestContext {
       ProxyRequestPriority priority__)
       : ProxyRequestContext(pr, priority__),
         proxy_(pr),
-        logger_(ProxyRequestLogger<RouterInfo>(pr)),
-        additionalLogger_(AdditionalLogger(*this)) {}
+        logger_(folly::in_place, pr),
+        additionalLogger_(folly::in_place, *this) {}
 
   Proxy<RouterInfo>& proxy_;
 
