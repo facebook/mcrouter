9989f82af910d39f5fb798eb2b459296a8f16d9f
Andre Pinto <aap@fb.com>
2018-10-16 12:08:13 -0700

    Fix travis-ci build
    Summary: Fixes the build in travis-ci.
    
    Reviewed By: orishu
    
    Differential Revision: D10391358
    
    fbshipit-source-id: ea5a8775bc478150691056144fef64689de9c2da

--- mcrouter/scripts/recipes/folly.sh
+++ mcrouter/scripts/recipes/folly.sh
@@ -15,20 +15,20 @@ if [[ ! -d folly ]]; then
 fi
 
 if [ ! -d /usr/include/double-conversion ]; then
-    if [ ! -d "$PKG_DIR/double-conversion" ]; then
-        cd "$PKG_DIR" || die "cd fail"
-        git clone https://github.com/google/double-conversion.git
-    fi
-    cd "$PKG_DIR/double-conversion" || die "cd fail"
+  if [ ! -d "$PKG_DIR/double-conversion" ]; then
+    cd "$PKG_DIR" || die "cd fail"
+    git clone https://github.com/google/double-conversion.git
+  fi
+  cd "$PKG_DIR/double-conversion" || die "cd fail"
 
-    # Workaround double-conversion CMakeLists.txt changes that
-    # are incompatible with cmake-2.8
-    git checkout ea970f69edacf66bd3cba2892be284b76e9599b0
-    cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR"
-    make "$MAKE_ARGS" && make install "$MAKE_ARGS"
+  # Workaround double-conversion CMakeLists.txt changes that
+  # are incompatible with cmake-2.8
+  git checkout ea970f69edacf66bd3cba2892be284b76e9599b0
+  cmake . -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR"
+  make $MAKE_ARGS && make install $MAKE_ARGS
 
-    export LDFLAGS="-L$INSTALL_DIR/lib -ldl $LDFLAGS"
-    export CPPFLAGS="-I$INSTALL_DIR/include $CPPFLAGS"
+  export LDFLAGS="-L$INSTALL_DIR/lib -ldl $LDFLAGS"
+  export CPPFLAGS="-I$INSTALL_DIR/include $CPPFLAGS"
 fi
 
 cd "$PKG_DIR/folly/folly/" || die "cd fail"
@@ -40,4 +40,4 @@ CXXFLAGS="$CXXFLAGS -fPIC" \
     -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
     -DCMAKE_INCLUDE_PATH="$INSTALL_DIR/lib" \
     -DCMAKE_LIBRARY_PATH="$INSTALL_DIR/lib"
-make "$MAKE_ARGS" && make install "$MAKE_ARGS"
+make $MAKE_ARGS && make install $MAKE_ARGS
