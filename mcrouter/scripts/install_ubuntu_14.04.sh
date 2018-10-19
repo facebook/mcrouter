5e3c4266e214484d15e0719d27a13a8163a952c7
Neel Goyal <ngoyal@fb.com>
2018-10-19 09:36:18 -0700

    Fix open source builds on ubuntu 14
    Summary: Mcrouter building on ubuntu 14 is broken due to fizz.  This attempts to fix some things.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D10446551
    
    fbshipit-source-id: b71cda4647f8eba6afd063c0b66ad76ea57d58b2

--- mcrouter/scripts/install_ubuntu_14.04.sh
+++ mcrouter/scripts/install_ubuntu_14.04.sh
@@ -7,6 +7,7 @@ set -ex
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo add-apt-repository -y ppa:chris-lea/libsodium
+sudo add-apt-repository -y ppa:sickpig/boost
 
 sudo apt-get update
 
@@ -20,7 +21,12 @@ sudo apt-get install -y \
     g++-5 \
     gcc \
     git \
-    libboost1.54-all-dev \
+    libboost1.58-dev \
+    libboost-thread1.58-dev \
+    libboost-filesystem1.58-dev \
+    libboost-context1.58-dev \
+    libboost-regex1.58-dev \
+    libboost-program-options1.58-dev \
     libdouble-conversion-dev \
     libevent-dev \
     libgflags-dev \
