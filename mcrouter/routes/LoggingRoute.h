1f656259d98b3c33c807885b45cfb4dbb47f5b2a
Kevin Lewi <klewi@fb.com>
2018-12-05 13:38:51 -0800

    Extending LoggingRoute to handle stored results
    Summary: Allows for the LoggingRoute callback to be triggered when isStoredResult() is true.
    
    Reviewed By: andreazevedo
    
    Differential Revision: D13330745
    
    fbshipit-source-id: fa1d33f8f63d2c6a44422ab700207534faaf110d

--- mcrouter/routes/LoggingRoute.h
+++ mcrouter/routes/LoggingRoute.h
@@ -1,9 +1,8 @@
-/*
- *  Copyright (c) 2015-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
 #pragma once
 
@@ -77,7 +76,7 @@ class LoggingRoute {
 
     auto& callback = ctx->proxy().router().postprocessCallback();
     if (callback) {
-      if (isHitResult(reply.result())) {
+      if (isHitResult(reply.result()) || isStoredResult(reply.result())) {
         callback(
             carbon::convertToFollyDynamic(req),
             carbon::convertToFollyDynamic(reply),
