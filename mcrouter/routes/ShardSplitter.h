45d44fbbfd817b71d7f5d184070d87d6a92eb5f8
Aditya Sarwade <asarwade@fb.com>
2018-12-19 23:20:30 -0800

    hashmap changes to save CPU and LLC misses
    Summary: as per title
    
    Reviewed By: binliu19
    
    Differential Revision: D7536437
    
    fbshipit-source-id: 3e561733d78225fc4bcfe2ed6e1b9d6a08ab465d

--- mcrouter/routes/ShardSplitter.h
+++ mcrouter/routes/ShardSplitter.h
@@ -1,16 +1,14 @@
-/*
- *  Copyright (c) 2014-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
 #pragma once
 
 #include <chrono>
 
 #include <folly/Range.h>
-#include <folly/experimental/StringKeyedUnorderedMap.h>
 
 #include "mcrouter/config.h"
 
@@ -93,12 +91,14 @@ class ShardSplitter {
    */
   const ShardSplitInfo& getShardSplit(folly::StringPiece shardId) const;
 
-  const folly::StringKeyedUnorderedMap<ShardSplitInfo>& getShardSplits() const {
+  using ShardSplitInfoMap = folly::F14FastMap<std::string, ShardSplitInfo>;
+
+  const ShardSplitInfoMap& getShardSplits() const {
     return shardSplits_;
   }
 
  private:
-  folly::StringKeyedUnorderedMap<ShardSplitInfo> shardSplits_;
+  ShardSplitInfoMap shardSplits_;
   ShardSplitInfo defaultShardSplit_;
   bool enablePrefixMatching_;
 };
