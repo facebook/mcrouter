1d6eb9457936c3962c5f8b9421397e1f0aa7299d
Ori Shalev <orish@fb.com>
2018-12-28 11:55:09 -0800

    Ubuntu packaging for v0.39.0 (#290)
    Summary:
    This commit includes the built binary deb (will be served from the git repo) and the docker-based build script changes that were required for building mcrouter 0.39.0 from source.
    Pull Request resolved: https://github.com/facebook/mcrouter/pull/290
    
    Reviewed By: andreazevedo
    
    Differential Revision: D13552071
    
    Pulled By: orishu
    
    fbshipit-source-id: 697dd4f949834b7e142c1f6447c2a64061bdcdca

--- packaging/xenial/dockerbuild/do-packaging.sh
+++ packaging/xenial/dockerbuild/do-packaging.sh
@@ -7,7 +7,7 @@ fi
 
 srcdir=$(dirname "$1")
 srctargz=$(basename "$1")
-origtargz=$(echo "$srctargz" | tr '-' '_' | sed -e 's/_release.tar.gz/.orig.tar.gz/')
+origtargz=$(echo "$srctargz" | tr '-' '_' | sed -e 's/.tar.gz/.orig.tar.gz/')
 fileprefix=${origtargz/[.]orig.tar.gz/}
 debiandir=$2
 dstdir=$3
@@ -15,7 +15,9 @@ dstdir=$3
 mcrouterdir=${srctargz/[.]tar.gz/}
 
 docker build -t mcrouter-xenial-packaging .
+docker run -i -d -v "$srcdir":/src -v "$debiandir":/debdir -v "$dstdir":/dst mcrouter-xenial-packaging /bin/bash -l
 cid=$(docker run -i -d -v "$srcdir":/src -v "$debiandir":/debdir -v "$dstdir":/dst mcrouter-xenial-packaging)
-docker exec -it "$cid" bash -l -c "cp /src/$srctargz $origtargz && tar zxf $origtargz && cd $mcrouterdir && mkdir debian && cp -R /debdir/* debian/ && debuild -us -uc && cp ../$fileprefix* /dst/"
+docker exec -it "$cid" bash -l -c "cp /src/$srctargz $origtargz && tar zxf $origtargz && cd $mcrouterdir && cp -R /debdir/* debian/ && rm -rf debian/{pool,db,dists} && debuild -us -uc && cp ../$fileprefix* /dst/"
 docker stop "$cid"
 docker rm "$cid"
+
