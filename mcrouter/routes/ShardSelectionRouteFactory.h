e259042efce9d253a53a1d232974a6abcdbdcffa
Andre Pinto <aap@fb.com>
2019-03-05 17:06:37 -0800

    Improve documentation of eager shard selection
    Summary:
    Add better documentation to the factory funciton of eager shard selection
    route handle.
    
    Reviewed By: glamtechie
    
    Differential Revision: D14209785
    
    fbshipit-source-id: 4927ad220f573da9cd88f9d2f3fca2b32adea521

--- mcrouter/routes/ShardSelectionRouteFactory.h
+++ mcrouter/routes/ShardSelectionRouteFactory.h
@@ -80,12 +80,16 @@ typename RouterInfo::RouteHandlePtr createShardSelectionRoute(
     const folly::dynamic& json);
 
 /**
- * Create a route handle for sharding requests to servers with an additional
- * children_type route that is applied to destinations mapped to shards.
+ * Creates a route handle that sends requests to destinations based on the
+ * shards map.
+ * If there are multiple servers serving the same shard, these destinations
+ * will be grouped together under the route handle specified by the
+ * "children_type" property.
  *
  * Sample json format:
  * {
  *   "children_type": "RouteHandle",
+ *   "children_settings" : { .. },
  *   "pools" : [
  *      {
  *        "pool": {
@@ -102,24 +106,26 @@ typename RouterInfo::RouteHandlePtr createShardSelectionRoute(
  *        },
  *      }
  *   ],
- *   "children_settings" : { .. },
  *   "out_of_range": "ErrorRoute"
  * }
  *
- * "shards" can be an array of strings of comma-separated
- * shard ids, as stated in ShardSelectionRoute above
- *
- * NOTE:
- *  - "shards" and "servers" must have the same number of entries, in exactly
- *    the same order (e.g. shards[5] shows the shards processed by servers[5]).
+ * DETAILS:
+ *  - "shards" can also be an array of strings of comma-separated shard ids,
+ *    as stated in the documentation of createShardSelectionRoute() above.
+ *  - "children_type" can be either LatestRoute or LoadBalancerRoute.
+ *  - "children_settings" is the properties of the route handle specified
+ *    in the "children_type" option.
+ *  - "pools" is an array of pools (or tiers).
+ *    - "shards" and "servers" must have the same number of entries, in exactly
+ *      the same order (e.g. shards[5] has the shards processed by servers[5]).
  *
  * @tparam ShardSelector Class responsible for selecting the shard responsible
  *                       for handling the request. The ShardSelector constructor
- *                       accepts a shardsMap (unordered_map) that maps
- *                       shardId -> destinationId.
- * @tparam MapType       C++ type container that maps shardId -> destinationId.
+ *                       takes a shardsMap (unordered_map) that maps
+ *                       shardId -> destinationIndex.
+ * @tparam MapType       C++ type container that maps shardId -> destinationIdx
  *
- * @param factory               RouteHandleFactory to create destinations.
+ * @param factory               RouteHandleFactory
  * @param json                  JSON object with RouteHandle representation.
  */
 template <
