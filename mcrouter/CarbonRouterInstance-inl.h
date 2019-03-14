52512086fea3edf2431abce93841253b1ee6078b
Feixiong Zhang <feixiong@fb.com>
2019-03-13 22:54:34 -0700

    Force mcrouter shutdown during proxygen forceStop
    Reviewed By: afrind
    
    Differential Revision: D14416536
    
    fbshipit-source-id: c3bb1790c7857827e0713bd6117d3851f4929755

--- mcrouter/CarbonRouterInstance-inl.h
+++ mcrouter/CarbonRouterInstance-inl.h
@@ -1,9 +1,8 @@
-/*
- *  Copyright (c) 2016-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
 #include <vector>
 
@@ -362,6 +361,11 @@ template <class RouterInfo>
 void CarbonRouterInstance<RouterInfo>::shutdown() noexcept {
   CHECK(!shutdownStarted_.exchange(true));
   shutdownImpl();
+}
+
+template <class RouterInfo>
+bool CarbonRouterInstance<RouterInfo>::isShutdown() const {
+  return shutdownStarted_.load();
 }
 
 template <class RouterInfo>
--- mcrouter/CarbonRouterInstance.h
+++ mcrouter/CarbonRouterInstance.h
@@ -1,9 +1,8 @@
-/*
- *  Copyright (c) 2016-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
 #pragma once
 
@@ -124,6 +123,11 @@ class CarbonRouterInstance
    * until all threads are stopped.
    */
   void shutdown() noexcept;
+
+  /**
+   * @return whether the router has been shutdown.
+   */
+  bool isShutdown() const;
 
   ProxyBase* getProxyBase(size_t index) const override;
 
