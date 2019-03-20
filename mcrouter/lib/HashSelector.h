0bdffaee5212e958407416d1a866c8a2e28920b3
Andre Pinto <aap@fb.com>
2019-03-20 12:31:35 -0700

    Extract generic parts of HashSelector
    Summary:
    Extract generic parts of HashSelector to HashSelectorBase. In the future, we
    will inherit from it to create route handles that hashes on things other than
    the key.
    
    Reviewed By: edenzik, stuclar
    
    Differential Revision: D14356406
    
    fbshipit-source-id: cede1199804763c49d3175429577f935f4a80cf3

--- mcrouter/lib/HashSelector.h
+++ mcrouter/lib/HashSelector.h
@@ -1,9 +1,8 @@
-/*
- *  Copyright (c) 2017-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
 #pragma once
 
@@ -19,9 +18,9 @@ namespace facebook {
 namespace memcache {
 
 template <class HashFunc>
-class HashSelector {
+class HashSelectorBase {
  public:
-  HashSelector(std::string salt, HashFunc hashFunc)
+  HashSelectorBase(std::string salt, HashFunc hashFunc)
       : salt_(std::move(salt)), hashFunc_(std::move(hashFunc)) {}
 
   std::string type() const {
@@ -29,38 +28,44 @@ class HashSelector {
         "hash|", HashFunc::type(), (salt_.empty() ? "" : "|salt=" + salt_));
   }
 
-  template <class Request>
-  size_t select(const Request& req, size_t size) const {
-    /* Hash functions can be stack-intensive,
-       so jump back to the main context */
-    return folly::fibers::runInMainContext([this, &req, size]() {
-      /* this-> here is necessary for gcc-4.7 - it can't find pick()
-         without it */
-      return this->selectInternal(req, size);
-    });
-  }
-
- private:
-  const std::string salt_;
-  const HashFunc hashFunc_;
-
-  template <class Request>
-  size_t selectInternal(const Request& req, size_t size) const {
+ protected:
+  template <class Key>
+  size_t selectInternal(Key&& key, size_t size) const {
     size_t n = 0;
     if (salt_.empty()) {
-      n = hashFunc_(req.key().routingKey());
+      n = hashFunc_(key);
     } else {
-      n = hashWithSalt(
-          req.key().routingKey(), salt_, [this](const folly::StringPiece sp) {
-            return hashFunc_(sp);
-          });
+      n = hashWithSalt(key, salt_, [this](const folly::StringPiece sp) {
+        return hashFunc_(sp);
+      });
     }
     if (UNLIKELY(n >= size)) {
       throw std::runtime_error("index out of range");
     }
     return n;
   }
+
+ private:
+  const std::string salt_;
+  const HashFunc hashFunc_;
 };
 
-} // memcache
-} // facebook
+template <class HashFunc>
+class HashSelector : public HashSelectorBase<HashFunc> {
+ public:
+  HashSelector(std::string salt, HashFunc hashFunc)
+      : HashSelectorBase<HashFunc>(std::move(salt), std::move(hashFunc)) {}
+
+  template <class Request>
+  size_t select(const Request& req, size_t size) const {
+    // Hash functions can be stack-intensive, so jump back to the main context
+    return folly::fibers::runInMainContext([this, &req, size]() {
+      // this-> here is necessary for gcc-4.7 - it can't find selectInternal()
+      // without it
+      return this->selectInternal(req.key().routingKey(), size);
+    });
+  }
+};
+
+} // namespace memcache
+} // namespace facebook
