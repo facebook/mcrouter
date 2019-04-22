fe8e184c996a32bebc7e00e03dbe8185e857c222
Jon Maltiel Swenson <jmswen@fb.com>
2019-04-22 14:26:21 -0700

    Add rsocket-cpp and yarpl as OSS dependencies
    Summary: fbthrift recently added yarpl and rsocket-cpp as explicit dependencies. Since mcrouter depends on fbthrift, the convenience installation scripts need to fetch and install the rsocket-cpp project (which contains yarpl).
    
    Reviewed By: glamtechie
    
    Differential Revision: D15017351
    
    fbshipit-source-id: 71fc08640fad0a728fb07b7c2095eb13e80150da

---
+++ mcrouter/scripts/recipes/rsocket-yarpl.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+if [[ ! -d "$PKG_DIR/rsocket-cpp" ]]; then
+  git clone https://github.com/rsocket/rsocket-cpp
+  cd "$PKG_DIR/rsocket-cpp" || die "cd fail"
+  if [[ -f "$REPO_BASE_DIR/mcrouter/RSOCKET_COMMIT" ]]; then
+    RSOCKET_COMMIT="$(head -n 1 "$REPO_BASE_DIR/mcrouter/RSOCKET_COMMIT")"
+    echo "RSOCKET_COMMIT file found: using rsocket-cpp commit $RSOCKET_COMMIT"
+    git checkout "$RSOCKET_COMMIT"
+  else
+    echo "No RSOCKET_COMMIT file, using rsocket-cpp HEAD=$(git rev-parse HEAD)"
+  fi
+fi
+
+mkdir -p "$PKG_DIR/rsocket-cpp/build"
+cd "$PKG_DIR/rsocket-cpp/build" || die "cd rsocket-cpp failed"
+
+CXXFLAGS="$CXXFLAGS -fPIC" \
+cmake .. -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+  -DBUILD_BENCHMARKS=OFF \
+  -DBUILD_EXAMPLES=OFF \
+  -DBUILD_TESTS=OFF
+make $MAKE_ARGS && make install $MAKE_ARGS
+
