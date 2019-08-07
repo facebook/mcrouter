c4ad8252c5f3c29f94b0083c7e043bb36e2cfe0e
Noah Moss <noahmoss@fb.com>
2019-08-07 16:58:24 -0700

    Add fallback route option for RateLimitRoute
    Summary: Adds support for an optional fallback route if the target route is rate limited.
    
    Differential Revision: D16452412
    
    fbshipit-source-id: dc7ba9d7f8af3e6328e1ad86b9be382a5cdd4018

--- mcrouter/routes/RateLimitRoute.h
+++ mcrouter/routes/RateLimitRoute.h
@@ -47,31 +47,43 @@ class RateLimitRoute {
   bool traverse(
       const Request& req,
       const RouteHandleTraverser<RouteHandleIf>& t) const {
+    // Traverse intentionally doesn't implement rate-limiting or fallback, to
+    // avoid changing state
     return t(*target_, req);
   }
 
-  RateLimitRoute(std::shared_ptr<RouteHandleIf> target, RateLimiter rl)
-      : target_(std::move(target)), rl_(std::move(rl)) {}
+  RateLimitRoute(
+      std::shared_ptr<RouteHandleIf> target,
+      RateLimiter rl,
+      std::shared_ptr<RouteHandleIf> fallback)
+      : target_(std::move(target)),
+        fallback_(std::move(fallback)),
+        rl_(std::move(rl)) {}
 
   template <class Request>
   ReplyT<Request> route(const Request& req) {
     if (LIKELY(rl_.canPassThrough<Request>())) {
       return target_->route(req);
     }
+    if (fallback_) {
+      return fallback_->route(req);
+    }
     return createReply(DefaultReply, req);
   }
 
  private:
   const std::shared_ptr<RouteHandleIf> target_;
+  const std::shared_ptr<RouteHandleIf> fallback_;
   RateLimiter rl_;
 };
 
 template <class RouteHandleIf>
 std::shared_ptr<RouteHandleIf> createRateLimitRoute(
     std::shared_ptr<RouteHandleIf> normalRoute,
-    RateLimiter rateLimiter) {
+    RateLimiter rateLimiter,
+    std::shared_ptr<RouteHandleIf> fallbackRoute = nullptr) {
   return makeRouteHandle<RouteHandleIf, RateLimitRoute>(
-      std::move(normalRoute), std::move(rateLimiter));
+      std::move(normalRoute), std::move(rateLimiter), std::move(fallbackRoute));
 }
 
 template <class RouteHandleIf>
@@ -84,9 +96,17 @@ std::shared_ptr<RouteHandleIf> makeRateLimitRoute(
   auto target = factory.create(*jtarget);
   auto jrates = json.get_ptr("rates");
   checkLogic(jrates, "RateLimitRoute: rates not found");
-  return createRateLimitRoute(std::move(target), RateLimiter(*jrates));
+  std::shared_ptr<RouteHandleIf> fallback;
+  if (auto jfallback = json.get_ptr("fallback")) {
+    checkLogic(
+        jfallback != jtarget,
+        "RateLimitRoute: target and fallback are the same");
+    fallback = factory.create(*jfallback);
+  }
+  return createRateLimitRoute(
+      std::move(target), RateLimiter(*jrates), std::move(fallback));
 }
 
-} // mcrouter
-} // memcache
-} // facebook
+} // namespace mcrouter
+} // namespace memcache
+} // namespace facebook
--- mcrouter/routes/test/RateLimitRouteTest.cpp
+++ mcrouter/routes/test/RateLimitRouteTest.cpp
@@ -35,23 +35,30 @@ namespace {
 template <class Data, class Request>
 void test(
     Data data,
+    Data fallbackData,
     Request,
     carbon::Result ok,
     carbon::Result reject,
+    carbon::Result fallbackRes,
     const std::string& type,
-    bool burst = false) {
+    bool burst = false,
+    bool fallback = false) {
   vector<std::shared_ptr<TestHandle>> normalHandle{
       make_shared<TestHandle>(std::move(data)),
   };
   auto normalRh = get_route_handles(normalHandle)[0];
 
+  vector<std::shared_ptr<TestHandle>> fallbackHandle{
+      make_shared<TestHandle>(std::move(fallbackData)),
+  };
+  auto fallbackRh = get_route_handles(fallbackHandle)[0];
+
   auto json = parseJsonString(
       (burst
            ? string("{\"") + type + "_rate\": 4.0, \"" + type + "_burst\": 3.0}"
            : string("{\"") + type + "_rate\": 2.0}"));
-
   McrouterRouteHandle<RateLimitRoute<McrouterRouteHandleIf>> rh(
-      normalRh, RateLimiter(json));
+      normalRh, RateLimiter(json), fallback ? fallbackRh : nullptr);
 
   Request req("key");
   // McSetRequest requires value be set; this is a no-op for Get and Delete
@@ -69,47 +76,56 @@ void test(
     reply = rh.route(req);
     EXPECT_EQ(reply.result(), ok);
     reply = rh.route(req);
-    EXPECT_EQ(reply.result(), reject);
+    EXPECT_EQ(reply.result(), fallback ? fallbackRes : reject);
   } else {
     usleep(501000);
     auto reply = rh.route(req);
     EXPECT_EQ(reply.result(), ok);
     reply = rh.route(req);
-    EXPECT_EQ(reply.result(), reject);
+    EXPECT_EQ(reply.result(), fallback ? fallbackRes : reject);
   }
 }
 
-void testSets(bool burst = false) {
+void testSets(bool burst = false, bool fallback = false) {
   test(
       UpdateRouteTestData(carbon::Result::STORED),
+      UpdateRouteTestData(carbon::Result::LOCAL_ERROR),
       McSetRequest(),
       carbon::Result::STORED,
       carbon::Result::NOTSTORED,
+      carbon::Result::LOCAL_ERROR,
       "sets",
-      burst);
+      burst,
+      fallback);
 }
 
-void testGets(bool burst = false) {
+void testGets(bool burst = false, bool fallback = false) {
   test(
       GetRouteTestData(carbon::Result::FOUND, "a"),
+      GetRouteTestData(carbon::Result::LOCAL_ERROR, "a"),
       McGetRequest(),
       carbon::Result::FOUND,
       carbon::Result::NOTFOUND,
+      carbon::Result::LOCAL_ERROR,
       "gets",
-      burst);
+      burst,
+      fallback);
 }
 
-void testDeletes(bool burst = false) {
+void testDeletes(bool burst = false, bool fallback = false) {
   test(
       DeleteRouteTestData(carbon::Result::DELETED),
+      DeleteRouteTestData(carbon::Result::LOCAL_ERROR),
       McDeleteRequest(),
       carbon::Result::DELETED,
       carbon::Result::NOTFOUND,
+      carbon::Result::LOCAL_ERROR,
       "deletes",
-      burst);
+      burst,
+      fallback);
 }
 
-} // anonymous
+} // namespace
 
 TEST(rateLimitRouteTest, setsBasic) {
   testSets();
@@ -117,15 +133,24 @@ TEST(rateLimitRouteTest, setsBasic) {
 TEST(rateLimitRouteTest, setsBurst) {
   testSets(true);
 }
+TEST(rateLimitRouteTest, setsFallback) {
+  testSets(false, true);
+}
 TEST(rateLimitRouteTest, getsBasic) {
   testGets();
 }
 TEST(rateLimitRouteTest, getsBurst) {
   testGets(true);
 }
+TEST(rateLimitRouteTest, getsFallback) {
+  testGets(false, true);
+}
 TEST(rateLimitRouteTest, deletesBasic) {
   testDeletes();
 }
 TEST(rateLimitRouteTest, deletesBurst) {
   testDeletes(true);
+}
+TEST(rateLimitRouteTest, deletesFallback) {
+  testDeletes(false, true);
 }
