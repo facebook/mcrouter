20ca8de0d66cc25b25da4e49f7618be4f7eae10d
Andres Suarez <asuarez@fb.com>
2019-10-14 10:51:37 -0700

    Tidy up license headers [1/3]
    Reviewed By: mzlee
    
    Differential Revision: D17901957
    
    fbshipit-source-id: 17c8a90e043a9cd522ca031668bc495731ce888e

--- mcrouter/lib/WeightedCh4HashFunc.cpp
+++ mcrouter/lib/WeightedCh4HashFunc.cpp
@@ -1,9 +1,10 @@
 /*
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the MIT license found in the LICENSE
- * file in the root directory of this source tree.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 #include "WeightedCh4HashFunc.h"
 
 #include "mcrouter/lib/fbi/WeightedFurcHash.h"
--- mcrouter/options-template.h
+++ mcrouter/options-template.h
@@ -1,9 +1,10 @@
 /*
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the MIT license found in the LICENSE
- * file in the root directory of this source tree.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 class OPTIONS_NAME : public McrouterOptionsBase {
  public:
 #define MCROUTER_STRING_MAP std::unordered_map<std::string, std::string>
