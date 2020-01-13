21a27b0f8efa73b11ea70bdec3f7b03af45d43e0
Huapeng Zhou <hzhou@fb.com>
2020-01-13 15:56:37 -0800

    proxygen: bump up stderr logging for mcrouter ProxyDestinationBase code
    Reviewed By: stuclar
    
    Differential Revision: D19377854
    
    fbshipit-source-id: 28a4db9c92bd105d470b7e070f4f6258843e7067

--- mcrouter/ProxyDestinationBase.cpp
+++ mcrouter/ProxyDestinationBase.cpp
@@ -92,7 +92,7 @@ bool ProxyDestinationBase::maySend(carbon::Result& tkoReason) const {
 void ProxyDestinationBase::onTkoEvent(TkoLogEvent event, carbon::Result result)
     const {
   auto logUtil = [this, result](folly::StringPiece eventStr) {
-    VLOG(3) << accessPoint_->toHostPortString() << " " << eventStr
+    VLOG(2) << accessPoint_->toHostPortString() << " " << eventStr
             << ". Total hard TKOs: " << tracker_->globalTkos().hardTkos
             << "; soft TKOs: " << tracker_->globalTkos().softTkos
             << ". Reply: " << carbon::resultToString(result);
