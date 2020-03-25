6f8c6d4c3791dcadd2b2e9e8fb09865fffb92079
Vrishal Kulkarni <vrishal@fb.com>
2020-03-25 10:31:18 -0700

    Add mcrouter stat for cross region policy for use in tests
    Summary: Add a count stat to count when all DB replicas are disabled for use in tests. Use better stat names
    
    Differential Revision: D20640353
    
    fbshipit-source-id: f479d643ddcd8a697838044036103f604c759771

--- mcrouter/stat_list.h
+++ mcrouter/stat_list.h
@@ -194,9 +194,9 @@ STUIR(failover_custom_master_region, 0, 1)
 STUIR(failover_custom_master_region_skipped, 0, 1)
 STUIR(failover_custom_master_region_invalid, 0, 1)
 STUIR(custom_policy_large_assoc_reqs, 0, 1)
-STUIR(failover_custom_skip_dbless_region, 0, 1)
-STUIR(failover_custom_db_regions_missing, 0, 1)
-STUIR(failover_custom_db_regions_empty, 0, 1)
+STUIR(failover_custom_db_disabled_region, 0, 1)
+STUIR(failover_custom_db_enabled_regions_missing, 0, 1)
+STUIR(failover_custom_db_disabled_all_regions, 0, 1)
 #undef GROUP
 #define GROUP ods_stats | count_stats
 STUI(result_error_count, 0, 1)
@@ -232,7 +232,8 @@ STUI(failover_custom_master_region_count, 0, 1)
 STUI(failover_custom_master_region_skipped_count, 0, 1)
 STUI(custom_policy_large_assoc_reqs_count, 0, 1)
 STUI(dest_with_no_failure_domain_count, 0, 1)
-STUI(failover_custom_skip_dbless_region_count, 0, 1)
+STUI(failover_custom_db_disabled_region_count, 0, 1)
+STUI(failover_custom_db_disabled_all_regions_count, 0, 1)
 #undef GROUP
 #define GROUP ods_stats | detailed_stats | rate_stats
 STUIR(final_result_error, 0, 1)
