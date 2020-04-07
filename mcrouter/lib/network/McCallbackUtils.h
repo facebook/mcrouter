ee698c0ec243cc9139e7356743eb6a418d3e0473
Stuart Clark <stuclar@fb.com>
2020-04-07 14:06:36 -0700

    Callback utils for thrift
    Summary: As above
    
    Reviewed By: spalamarchuk
    
    Differential Revision: D20359294
    
    fbshipit-source-id: 4d9a43eb83dd08527104a020586d73a05b133482

---
+++ mcrouter/lib/network/McCallbackUtils.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <folly/Optional.h>
+#include <folly/io/async/EventBase.h>
+
+#include <mcrouter/lib/network/gen/gen-cpp2/Memcache.h>
+
+namespace facebook {
+namespace memcache {
+
+template <class Reply>
+class McThriftCallback {
+ public:
+  McThriftCallback(std::unique_ptr<apache::thrift::HandlerCallback<Reply>> ctx)
+      : underlying_(std::move(ctx)) {}
+
+  static void reply(
+      McThriftCallback<Reply>&& ctx,
+      Reply&& reply,
+      bool /* flush */ = false) {
+    decltype(ctx.underlying_)::element_type::resultInThread(
+        std::move(ctx.underlying_), std::move(reply));
+  }
+
+  folly::Optional<struct sockaddr_storage> getPeerSocketAddress() {
+    folly::Optional<struct sockaddr_storage> peerAddress;
+    auto connectionCxt = underlying_->getConnectionContext();
+    if (connectionCxt && connectionCxt->getPeerAddress()) {
+      peerAddress.emplace();
+      connectionCxt->getPeerAddress()->getAddress(peerAddress.get_pointer());
+    }
+    return peerAddress;
+  }
+
+  folly::EventBase& getSessionEventBase() const noexcept {
+    return *(underlying_->getEventBase());
+  }
+
+ private:
+  std::unique_ptr<apache::thrift::HandlerCallback<Reply>> underlying_;
+};
+
+} // namespace memcache
+} // namespace facebook
