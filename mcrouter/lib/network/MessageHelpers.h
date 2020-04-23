b6213ef6b69f7e1d2fd272b17a0886cf08d68842
Stuart Clark <stuclar@fb.com>
2020-04-23 15:29:41 -0700

    Add getFlagsIfExist and getExptimeIfExist APIs
    Summary: Added functions that use traits to return flags and exptime if they exist
    
    Reviewed By: spalamarchuk
    
    Differential Revision: D21022100
    
    fbshipit-source-id: 6057973b065445340655bea72e260bdc16aff0e2

--- mcrouter/lib/network/MessageHelpers.h
+++ mcrouter/lib/network/MessageHelpers.h
@@ -22,6 +22,14 @@ void setFlagsImpl(std::true_type, Message& message, uint64_t flags) {
 }
 template <typename Message>
 void setFlagsImpl(std::false_type, Message&, uint64_t) {}
+template <typename Message>
+uint64_t getFlagsImpl(std::true_type, Message& message) {
+  return message.flags();
+}
+template <typename Message>
+uint64_t getFlagsImpl(std::false_type, Message&) {
+  return 0;
+}
 // exptime
 template <class Message>
 void setExptimeImpl(std::true_type, Message& message, int32_t exptime) {
@@ -29,6 +37,14 @@ void setExptimeImpl(std::true_type, Message& message, int32_t exptime) {
 }
 template <class Message>
 void setExptimeImpl(std::false_type, Message&, int32_t) {}
+template <class Message>
+int32_t getExptimeImpl(std::true_type, Message& message) {
+  return message.exptime();
+}
+template <class Message>
+int32_t getExptimeImpl(std::false_type, Message&) {
+  return 0;
+}
 // value
 template <class Message>
 void setValueImpl(std::true_type, Message& message, folly::IOBuf&& buf) {
@@ -51,6 +67,11 @@ template <class Message>
 void setFlags(Message& message, uint64_t flags) {
   detail::setFlagsImpl(HasFlagsTrait<Message>{}, message, flags);
 }
+// Return flags if it exists in Message and 0 otherwise.
+template <class Message>
+uint64_t getFlagsIfExist(Message& message) {
+  return detail::getFlagsImpl(HasFlagsTrait<Message>{}, message);
+}
 
 // Exptime helpers
 template <typename Message, typename = std::enable_if_t<true>>
@@ -64,6 +85,11 @@ class HasExptimeTrait<
 template <class Message>
 void setExptime(Message& message, int32_t exptime) {
   detail::setExptimeImpl(HasExptimeTrait<Message>{}, message, exptime);
+}
+// Return exptime if it exists in Message and 0 otherwise.
+template <class Message>
+int32_t getExptimeIfExist(Message& message) {
+  return detail::getExptimeImpl(HasExptimeTrait<Message>{}, message);
 }
 
 // Value helpers
