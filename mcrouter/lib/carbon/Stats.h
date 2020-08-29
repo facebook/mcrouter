a4191b64fa72cafe1950736982f9d1912c3263b7
Robin Cheng <hpmv@fb.com>
2020-08-28 17:58:10 -0700

    Fix a race condition in carbon stats.
    Summary:
    Stats in mcrouter are not being used in a consistently thread-safe way. This fixes one of the two very widely occurring TSAN-reported race conditions. See example stack trace here: P137820660
    
    In an ideal world, stat structures should not have any thread safety built-in, because they are more like PODs. Users of these structures should do their own synchronization (such as, accumulating stats in a request thread and then at the very end commit to a global set of stats). However, the users of this stats structure does not seem to (consistenly) respect thread safety, and since I don't understand how the mcrouter code works, I don't have the ability to refactor stats handling at a higher level.
    
    Also, a lot of existing stats are already being read/written as atomics to avoid race conditions (see for example fbcode/mcrouter/stats.h:153), so I felt that it's OK to just make these stats atomic too.
    
    Please let me know if you prefer different way of fixing this. Thanks!
    
    Reviewed By: stuclar
    
    Differential Revision: D23183149
    
    fbshipit-source-id: 92216c5cac5043ecb469a427cd8051005331f281

--- mcrouter/lib/carbon/Stats.h
+++ mcrouter/lib/carbon/Stats.h
@@ -80,16 +80,19 @@ class Stats {
         return;
       }
       constexpr auto statGroup = StatsConfig::template getStatGroup<Request>();
-      ++sumStats_[statGroup];
+      auto& stat = sumStats_[statGroup];
+      stat.store(
+          stat.load(std::memory_order_relaxed) + 1, std::memory_order_relaxed);
     }
 
     folly::dynamic dump(bool filterZeroes) const {
       folly::dynamic rv(folly::dynamic::object());
       for (size_t statGroup = 0; statGroup < kNumStats; ++statGroup) {
-        if (filterZeroes && sumStats_[statGroup] == 0) {
+        auto sumStat = sumStats_[statGroup].load(std::memory_order_relaxed);
+        if (filterZeroes && sumStat == 0) {
           continue;
         }
-        rv[kStatNames[statGroup]] = sumStats_[statGroup];
+        rv[kStatNames[statGroup]] = sumStat;
       }
       return rv;
     }
@@ -102,7 +105,7 @@ class Stats {
     static constexpr std::array<folly::StringPiece, kNumStats> kStatNames =
         StatsConfig::sumStatNames;
 
-    std::array<uint64_t, kNumStats> sumStats_{};
+    std::array<std::atomic<uint64_t>, kNumStats> sumStats_{};
   };
 
   class RateStats {
@@ -119,7 +122,9 @@ class Stats {
       constexpr auto statGroup = StatsConfig::template getStatGroup<Request>();
       const size_t baseOffset =
           static_cast<size_t>(statType) * kNumRequestGroups;
-      ++currentStats_[baseOffset + statGroup];
+      auto& stat = currentStats_[baseOffset + statGroup];
+      stat.store(
+          stat.load(std::memory_order_relaxed) + 1, std::memory_order_relaxed);
     }
 
     folly::dynamic dump(bool filterZeroes) const {
@@ -144,9 +149,11 @@ class Stats {
 
       for (size_t statIndex = 0; statIndex < kNumStats; ++statIndex) {
         accumulatedStats_[statIndex] -= rateStats_[statIndex][binIndex_];
-        rateStats_[statIndex][binIndex_] = currentStats_[statIndex];
-        accumulatedStats_[statIndex] += currentStats_[statIndex];
-        currentStats_[statIndex] = 0;
+        auto currentStat =
+            currentStats_[statIndex].load(std::memory_order_relaxed);
+        rateStats_[statIndex][binIndex_] = currentStat;
+        accumulatedStats_[statIndex] += currentStat;
+        currentStats_[statIndex].store(0, std::memory_order_relaxed);
       }
     }
 
@@ -162,7 +169,7 @@ class Stats {
 
     size_t binIndex_{0};
     size_t numBinsUsed_{0};
-    std::array<uint64_t, kNumStats> currentStats_{};
+    std::array<std::atomic<uint64_t>, kNumStats> currentStats_{};
     std::array<uint64_t, kNumStats> accumulatedStats_{};
     std::array<std::array<uint64_t, kNumBins>, kNumStats> rateStats_{};
   };
