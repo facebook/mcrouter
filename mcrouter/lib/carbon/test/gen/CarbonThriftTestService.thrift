8eb73acbfba7d17378fddbfd6c9831537bcd82c6
Stuart Clark <stuclar@fb.com>
2020-09-25 16:13:56 -0700

    Add support for Thrift TM threading model.
    Summary: As above
    
    Reviewed By: jmswen
    
    Differential Revision: D23928454
    
    fbshipit-source-id: b0522d2dd3e94efb01f7bdbfde4142a46344e1d0

--- mcrouter/lib/carbon/test/gen/CarbonThriftTestService.thrift
+++ mcrouter/lib/carbon/test/gen/CarbonThriftTestService.thrift
@@ -20,7 +20,7 @@ cpp_include "mcrouter/lib/carbon/test/gen/CarbonThriftTestMessages.h"
 namespace cpp2 carbon.test.thrift
 
 service CarbonThriftTest {
-  CarbonThriftTest.DummyThriftReply (cpp.type = "carbon::test::DummyThriftReply") thrift_test(1: CarbonThriftTest.DummyThriftRequest (cpp.type = "carbon::test::DummyThriftRequest") request) (thread = "eb")
-  CarbonThriftTest.ThriftTestReply (cpp.type = "carbon::test::ThriftTestReply") test(1: CarbonThriftTest.ThriftTestRequest (cpp.type = "carbon::test::ThriftTestRequest") request) (thread = "eb")
-  Common.McVersionReply (cpp.type = "facebook::memcache::McVersionReply") mcVersion(1: Common.McVersionRequest (cpp.type = "facebook::memcache::McVersionRequest") request) (thread = "eb")
+  CarbonThriftTest.DummyThriftReply (cpp.type = "carbon::test::DummyThriftReply") thrift_test(1: CarbonThriftTest.DummyThriftRequest (cpp.type = "carbon::test::DummyThriftRequest") request)
+  CarbonThriftTest.ThriftTestReply (cpp.type = "carbon::test::ThriftTestReply") test(1: CarbonThriftTest.ThriftTestRequest (cpp.type = "carbon::test::ThriftTestRequest") request)
+  Common.McVersionReply (cpp.type = "facebook::memcache::McVersionReply") mcVersion(1: Common.McVersionRequest (cpp.type = "facebook::memcache::McVersionRequest") request)
 }
