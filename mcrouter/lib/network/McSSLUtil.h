1419b03a278024697998f86a4ac5aa26db7b4ada
Eden Zik <ezik@fb.com>
2021-08-11 16:49:30 -0700

    Dropping x509 payload in Carbon after connection establishment
    Summary:
    This change is intended to reduce TAO's connection memory overhead during failover, which is captured in S238048.
    
    This takes place only in Carbon. Specifically, we:
    - Create a new Thrift conn context. The impl formerly held on to the x509 cert until it was removed in D29685651
    - Trim the certificate after connection is accepted using fb-specific API (` SSL_fb_clear_peer_certificate`)
    
    Reviewed By: mingtaoy
    
    Differential Revision: D30201107
    
    fbshipit-source-id: 2329a7afff639225ee6d2520716536d3fa13413b

--- mcrouter/lib/network/McSSLUtil.h
+++ mcrouter/lib/network/McSSLUtil.h
@@ -28,6 +28,9 @@ class McSSLUtil {
       folly::Function<bool(folly::AsyncSSLSocket*, bool, X509_STORE_CTX*)
                           const noexcept>;
 
+  using DropCertificateX509PayloadFunction =
+      folly::Function<bool(folly::AsyncSSLSocket&) const noexcept>;
+
   static const std::string kTlsToPlainProtocolName;
 
   static bool verifySSLWithDefaultBehavior(
@@ -67,6 +70,12 @@ class McSSLUtil {
    */
   static void setClientIdentityHook(apache::thrift::ClientIdentityHook func);
 
+  /*
+   * Certificate x509 payload drop function
+   */
+  static void setDropCertificateX509PayloadFunction(
+      DropCertificateX509PayloadFunction func);
+
   /**
    * Verify an SSL connection.  If no application verifier is set, the default
    * verifier is used.
@@ -92,6 +101,11 @@ class McSSLUtil {
    */
   static folly::AsyncTransportWrapper::UniquePtr moveToPlaintext(
       folly::AsyncSSLSocket& sock) noexcept;
+
+  /**
+   * Drops certificate x509 payload after connection is established
+   */
+  static void dropCertificateX509Payload(folly::AsyncSSLSocket& sock) noexcept;
 
   /**
    * Move the existing transport to kTLS if possible.  Return value of
