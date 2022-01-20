4e08bf01b8ef8a83f598f428d5c929d92c858f14
Luke Ye <lukeye@fb.com>
2022-01-19 17:06:38 -0800

    Update license headers for Meta
    Summary: As title
    
    Reviewed By: zertosh, Happytreat
    
    Differential Revision: D33646066
    
    fbshipit-source-id: 9e5961a9ce21ea4dbc2bc3df7febca70e6d955ac

--- mcrouter/lib/fbi/hash.h
+++ mcrouter/lib/fbi/hash.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
@@ -39,7 +39,7 @@ __BEGIN_DECLS
  * This is adapted from the fbomb source, which included the
  * following copyright notice:
  *
- * Copyright (c) 2009- Facebook -- All Rights Reserved
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  */
 
 uint32_t furc_hash(const char* const key, const size_t len, const uint32_t m);
