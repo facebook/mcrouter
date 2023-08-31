f07d090f92f9fea104cc0481601baac66d0458fc
lenar-f <108102975+lenar-f@users.noreply.github.com>
2023-08-31 11:48:15 -0700

    mvfst.sh

--- mcrouter/scripts/recipes/mvfst.sh
+++ mcrouter/scripts/recipes/mvfst.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
 
 source common.sh
 
@@ -13,4 +9,3 @@ if [ ! -d "$PKG_DIR/mvfst" ]; then
     cmake . \
         -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_TESTS=OFF
     make $MAKE_ARGS && make install $MAKE_ARGS
-fi
