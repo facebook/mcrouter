1f4ca73f8e936fd016c1f9d658d087c26ab93ea7
lenar-f <108102975+lenar-f@users.noreply.github.com>
2023-08-31 11:52:33 -0700

    mvfst.sh(2)

---
+++ mcrouter/scripts/recipes/mvfst.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+source common.sh
+
+if [ ! -d "$PKG_DIR/mvfst" ]; then
+    git clone https://github.com/facebook/mvfst.git "$PKG_DIR/mvfst"
+    cd "$PKG_DIR/mvfst" || die "cd fail"
+
+    cmake . \
+        -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DBUILD_TESTS=OFF
+    make $MAKE_ARGS && make install $MAKE_ARGS
